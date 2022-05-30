e7c7270ed6a5ddbfa94706ea6eadf38b369b26d3
1000marin <leexjin@gmail.com>
2022-05-30 15:09:23 +0900

    BjPython

--- 2_7.py
+++ 2_7.py
@@ -4,7 +4,7 @@ if a==b==c:
 elif a==b or a==c:
     m = 1000 + (a * 100)
 elif b==c:
-    m = 1000 + (a * 100)
+    m = 1000 + (b * 100)
 else:
     max=(a>b) and a or b    # a가 b보다 크면 max=a, a가b보다 크지않으면 max=b
     m=(c>max) and c or max  # c가 max보다 크면 max=c, c가max보다 크지 않으면 max=max
